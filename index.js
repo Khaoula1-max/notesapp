c784168f197bd535900540edbb0e488346841938
khaoula hassoune <khaoulahassoune1@gmail.com>
2024-12-04 11:18:38 +0100

    Updated index.js to use require instead of import, added express.json middleware, and changed port configuration. Modified package.json to add start and dev scripts.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-import express from 'express';
+const express = require('express');
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('ahsan khaoula fl3alaaam');   
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port http://localhost:3000');
-});
+const port = process.env.PORT || 7856;
+app .use(express.json());
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+    
+}   );
