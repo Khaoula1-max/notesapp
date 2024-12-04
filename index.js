f1997cc71a492a08b6535027c94c82d5e3030f10
khaoula hassoune <khaoulahassoune1@gmail.com>
2024-12-04 11:25:56 +0100

    second commit

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 7856;
-app .use(express.json());
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-    
-}   );
