234b446cedb9ea6f81da8511df43905ab6e18bd7
khaoula hassoune <khaoulahassoune1@gmail.com>
2024-12-17 09:13:10 +0100

    `Added new functionality to NoteController.js and NoteRoutes.js for managing tags, categories and updated existing functionality for tags`

--- routes/NoteRoutes.js
+++ routes/NoteRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { addTags } = require('../controllers/NoteController');
+const { addTags,getTags } = require('../controllers/NoteController');
+const {validations,handleValidationErrors}= require ('../utils/validator')
 
-router.post('/tags', addTags);
+outer.post('/tags', validations.tagValidation, handleValidationErrors, addTags);
+router.get('/tags', getTags);
+router.get('/tags/:id', getTagById);
+router.put('/tags/:id', validations.tagValidation, handleValidationErrors, updateTag);
+router.delete('/tags/:tagId', deleteTag);
 
 module.exports = router;
--- utils/validation.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const { body , validationResult } = require('express-validator');
-const app = express();
- app.use (express.json());
- 
-
